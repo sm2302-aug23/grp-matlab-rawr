d1e67bbea7735f098ff6eb87f9e7d16e1994960d
Mimi Hassan <141210568+mimihassan@users.noreply.github.com>
2023-11-07 00:09:52 +0800

    test.m

--- plotWord.m
+++ plotWord.m
@@ -1,3 +1,5 @@
+function plotWord(input_string)
+
 % cursiveData from Task 1
 x_m_1 = [0 1.0 1.4 1.6 1.4 2.1 2.2 2.3 3.5 3.4 3.5 3.7 4.6];
 y_m_1 = [0 2.2 3.6 4.1 3.6 1.8 1.2 1.7 3.6 4.1 3.6 2.2 0.0];
@@ -17,33 +19,46 @@ y_n_2 = [18.4 15 18 18.4 17 15.3 15 15.7];
 x_i_4 = [2 2.5 3.0 2.8 2.6 2.7 3.0 3.4 3.8];
 y_i_4 = [11 11.2 13.0 11 9 8.4 8 8.4 9.8];
 
-% Defining the map
-cursiveData = containers.Map;
-cursiveData('M') = [x_m_1, y_m_1];
-cursiveData('a') = [x_a_1, y_a_1];
-cursiveData('h') = [x_h, y_h];
-cursiveData('r') = [x_r, y_r];
-cursiveData('n') = [x_n_2, y_n_2];
-cursiveData('i') = [x_i_4, y_i_4];
+    % Defining the map
+    cursiveData = containers.Map;
+    cursiveData('M') = {x_m_1, y_m_1};
+    cursiveData('a') = {x_a_1, y_a_1};
+    cursiveData('h') = {x_h, y_h};
+    cursiveData('r') = {x_r, y_r};
+    cursiveData('n') = {x_n_2, y_n_2};
+    cursiveData('i') = {x_i_4, y_i_4};
 
-function plotWord(inputWord, cursiveData)
-    combinedData = [];
+    % Convert the input string to lowercase
+    input_string = lower(input_string);
     
-    for i = 1:length(inputWord)
-        letter = inputWord(i);
-        
-        if isKey(cursiveData, letter)
-            letterData = cursiveData(letter);
-            combinedData = [combinedData; letterData];
+    % Initialize an empty string to hold the cursive-written word
+    cursive_word = '';
+
+    % Iterate over each character in the input string
+    for i = 1:length(input_string)
+        % Check if the character is in the cursive_letters map
+        if isKey(cursiveData, input_string(i))
+            % If yes, retrieve the coordinates and plot the letter
+            letter_coords = cursiveData(input_string(i));
+            for j = 1:length(letter_coords)
+                x = letter_coords{1};
+                y = letter_coords{2};
+                plot(x, y, 'k', 'LineWidth', 1.5);
+                hold on;
+            end
         else
-            error(['Cursive data not available for letter: ' letter]);
+            % If not, simply append the original character
+            cursive_word = strcat(cursive_word, input_string(i));
         end
     end
-
-    plot(combinedData(:, 1), combinedData(:, 2), 'b-', 'LineWidth', 2);
-    title(inputWord);
+    
+    % Set plot settings
     axis equal;
-    xlabel('X-coordinate');
-    ylabel('Y-coordinate');
+    axis off;
+    title('Cursive-written Word');
+    hold off;
+
 end
+% call to function
+% plotWord('Maharani');
 
